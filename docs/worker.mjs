d20a78450137a946d526b0ebdea0017250fe492a
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-03-25 19:43:10 -0400

    Update worker.mjs

--- docs/worker.mjs
+++ docs/worker.mjs
@@ -5,6 +5,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 import * as Imported from "delayed.mjs";
 
+self.postMessage("module worker started");
+
 self.addEventListener("message", (evt) => {
   self.postMessage("Hello World!");
 });
