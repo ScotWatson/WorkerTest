3be3f12d9af4900318d05f0d920b7fa1126b9e00
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-03-26 06:08:44 -0400

    Update message.mjs

--- docs/message.mjs
+++ docs/message.mjs
@@ -3,6 +3,11 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-importScripts("https://scotwatson.github.io/WebCommon/MessageQueue@20250212.js");
+try {
+  importScripts("https://scotwatson.github.io/WebCommon/MessageQueue@20250212.js");
+} catch (err) {
+  console.error(err);
+}
+
 export class MessageQueue;
-export const myQueue = MessageQueue(self);
+export const myQueue = new MessageQueue(self);
