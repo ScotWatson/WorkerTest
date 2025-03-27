b199f8804f7496a2b786fc98ee04e44d48490444
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-03-26 20:21:14 -0400

    Create workerMain.mjs

---
+++ docs/workerMain.mjs
@@ -0,0 +1,11 @@
+/*
+(c) 2025 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+import * as MessageQueue from "https://scotwatson.github.io/WebCommon/MessageQueue@20250212.mjs";
+export * as Imported from "./delayed.mjs";
+
+// Allows starting and stopping the main port
+// The port provided by WHATWG does not provide this capability.
+export port = new MessageQueue(self);
