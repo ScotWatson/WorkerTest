fade2a508a192712279c97a39ef2c690a9a4b790
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-03-29 19:53:52 -0400

    Update classic.js

--- docs/classic.js
+++ docs/classic.js
@@ -3,7 +3,9 @@
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-console.log("start classic");
-const start = performance.now();
-while (performance.now() < start + 3000);
-console.log("end classic");
+(() => {
+  console.log("start classic");
+  const start = performance.now();
+  while (performance.now() < start + 3000);
+  console.log("end classic");
+})();
