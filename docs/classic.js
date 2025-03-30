487e9224182df0e63757fda79320cccf9452221d
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2025-03-30 10:55:41 -0400

    Update classic.js

--- docs/classic.js
+++ docs/classic.js
@@ -4,10 +4,15 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 */
 
 (() => {
-  window.queueMicroTask(() => {
-    console.log(Date.now(), "microtask execute");
-  });
+//  window.queueMicroTask(() => {
+//    console.log(Date.now(), "microtask execute");
+//  });
   console.log(Date.now(), "start classic");
+  const props = [];
+  for (const prop in window) {
+    props.push(prop);
+  }
+  console.log(props);
   const start = performance.now();
   while (performance.now() < start + 3000);
   console.log(Date.now(), "end classic");
